91d4fd2642b1c80616329dede245a1e70eff5168
danushka9999 <dranasinghe99@gmail.com>
2021-07-28 17:04:05 +0530

    Correct the folder structure

---
+++ lessonWork/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+const Hello = (props) => {
+  return (
+    <>
+       Hello {props.name}, you are {props.age} years old
+    </>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </>
+  )
+}
+export default App
+
