ff69896e32485fcc18db1408dcbfc5368ab26f63
danushka9999 <dranasinghe99@gmail.com>
2021-07-29 19:34:07 +0530

    Event handlers upgraded

--- part1/lessonWork/src/App.js
+++ part1/lessonWork/src/App.js
@@ -3,15 +3,14 @@ import React, { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  const handleClick = () => {
-    console.log('clicked')
-  }
-
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={handleClick}>
+      <button onClick={() => setCounter(counter +1)}>
         plus
+      </button>
+      <button onClick={() => setCounter(0)}> 
+        zero
       </button>
     </div>
   )
